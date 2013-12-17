b472029146eaf56ecf9c96ef7cd5f9f57af6d2e0
Adrien SAUVEZ <adrien.sauvez@openwide.fr>
2013-12-17 10:54:04 +0100

    Supprime warning dans pom et améliore création node simple.

--- owsi-alfresco/owsi-alfresco-app/owsi-alfresco-app-core/src/main/java/fr/openwide/alfresco/app/core/node/service/NodeService.java
+++ owsi-alfresco/owsi-alfresco-app/owsi-alfresco-app-core/src/main/java/fr/openwide/alfresco/app/core/node/service/NodeService.java
@@ -1,6 +1,9 @@
 package fr.openwide.alfresco.app.core.node.service;
 
+import java.io.IOException;
 import java.io.InputStream;
+
+import org.springframework.web.multipart.MultipartFile;
 
 import fr.openwide.alfresco.repository.api.node.exception.DuplicateChildNameException;
 import fr.openwide.alfresco.repository.api.node.service.NodeRemoteService;
@@ -8,5 +11,9 @@ import fr.openwide.alfresco.repository.api.remote.model.NodeReference;
 
 public interface NodeService extends NodeRemoteService {
 
+	NodeReference createFolder(NodeReference parent, String folderName) throws DuplicateChildNameException;
+
 	NodeReference createContent(NodeReference parent, String fileName, String mimeType, String encoding, InputStream content) throws DuplicateChildNameException;
+
+	NodeReference createContent(NodeReference parent, MultipartFile file) throws DuplicateChildNameException, IOException;
 }
