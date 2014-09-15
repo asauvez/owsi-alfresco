a583ee9b7bcd4e30436a755f103a39c11ae23add
Adrien SAUVEZ <adrien.sauvez@openwide.fr>
2014-09-15 10:02:55 +0200

    IntegrityRemoteException

---
+++ owsi-alfresco/owsi-alfresco-repo/owsi-alfresco-repo-api/src/main/java/fr/openwide/alfresco/repository/api/remote/exception/IntegrityRemoteException.java
@@ -0,0 +1,15 @@
+package fr.openwide.alfresco.repository.api.remote.exception;
+
+public class IntegrityRemoteException extends RepositoryRemoteException {
+
+	private static final long serialVersionUID = 5301662560426323353L;
+
+	public IntegrityRemoteException() {
+		super();
+	}
+
+	public IntegrityRemoteException(Throwable cause) {
+		super(cause);
+	}
+
+}
