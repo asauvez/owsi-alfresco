01930cb5ba8257a96259a893501946762a715036
Adrien SAUVEZ <adrien.sauvez@openwide.fr>
2015-06-22 12:35:23 +0200

    Serialization de RepoSortDefinition

--- owsi-alfresco/owsi-alfresco-api/owsi-alfresco-api-core/src/main/java/fr/openwide/alfresco/api/core/search/model/RepositorySortDefinition.java
+++ owsi-alfresco/owsi-alfresco-api/owsi-alfresco-api-core/src/main/java/fr/openwide/alfresco/api/core/search/model/RepositorySortDefinition.java
@@ -2,14 +2,20 @@ package fr.openwide.alfresco.api.core.search.model;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import fr.openwide.alfresco.api.core.remote.model.NameReference;
 
 public class RepositorySortDefinition implements Serializable {
 	
 	private final NameReference property;
 	private final boolean ascending;
-	
-	public RepositorySortDefinition(NameReference property, boolean ascending) {
+
+	@JsonCreator
+	public RepositorySortDefinition(
+			@JsonProperty("property") NameReference property, 
+			@JsonProperty("ascending") boolean ascending) {
 		this.property = property;
 		this.ascending = ascending;
 	}
