2de5e7fe87748c64fa564bd7ea2afb4b8b4a71e2
Adrien SAUVEZ <adrien.sauvez@openwide.fr>
2015-07-08 17:02:08 +0200

    Utilitaire pour vérifier les modèles Java

---
+++ owsi-alfresco/owsi-alfresco-component/owsi-alfresco-component-model/src/main/java/fr/openwide/alfresco/component/model/node/model/ModelChecker.java
@@ -0,0 +1,62 @@
+package fr.openwide.alfresco.component.model.node.model;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.openwide.alfresco.component.model.node.model.property.PropertyModel;
+import fr.openwide.alfresco.component.model.repository.model.CmModel;
+import fr.openwide.alfresco.component.model.repository.model.RnModel;
+import fr.openwide.alfresco.component.model.repository.model.SysModel;
+
+public class ModelChecker {
+	
+	private List<String> errors = new ArrayList<>();
+	
+	public void checkModel(Class<?> model) throws Exception {
+		String nameSpace = (String) model.getField("NAMESPACE").get(null);
+		
+		for (Field field : model.getFields()) {
+			Object value = field.get(null);
+			if (value instanceof ContainerModel) {
+				ContainerModel container = (ContainerModel) value;
+				if (! container.getNameReference().getNamespace().equals(nameSpace)) {
+					errors.add(model.getSimpleName() + " : Field NAMESPACE " + nameSpace + " is not the same as the container namespace " + container.getNameReference());
+				}
+				if (! container.getNameReference().getName().equals(field.getName())) {
+					errors.add(model.getSimpleName() + " : Field name " + field.getName() + " is not the same as the container name " + container.getNameReference().getName());
+				}
+				checkContainer(nameSpace, container);
+			}
+		}
+	}
+	
+	private void checkContainer(String nameSpace, ContainerModel container) throws Exception {
+		for (Field field : container.getClass().getFields()) {
+			if (field.getDeclaringClass() == container.getClass()) {
+				Object value = field.get(container);
+				if (value instanceof PropertyModel) {
+					PropertyModel<?> property = (PropertyModel<?>) value;
+					if (! container.getNameReference().getNamespace().equals(nameSpace)) {
+						errors.add(container.getClass().getSimpleName() + " : Field NAMESPACE " + nameSpace + " is not the same as the property namespace " + property.getNameReference());
+					}
+					if (! property.getNameReference().getName().equals(field.getName())) {
+						errors.add(container.getClass().getSimpleName() + " : Field name " + field.getName() + " is not the same as the property name " + property.getNameReference().getName());
+					}
+				}
+			}
+		}
+	}
+	
+	public String getErrors() {
+		return errors.toString().replace(", ", "\n").replace("[", "").replace("]", "");
+	}
+	
+	public static void main(String args[]) throws Exception {
+		ModelChecker checker = new ModelChecker();
+		checker.checkModel(SysModel.class);
+		checker.checkModel(CmModel.class);
+		checker.checkModel(RnModel.class);
+		System.out.println(checker.getErrors());
+	}
+}
