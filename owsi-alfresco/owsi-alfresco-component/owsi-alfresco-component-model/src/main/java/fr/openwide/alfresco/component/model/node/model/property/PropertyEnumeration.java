997a4bf55c81a1b60a3a23bd56435224a4c20e7b
Adrien SAUVEZ <adrien.sauvez@openwide.fr>
2015-08-12 17:28:05 +0200

    EnumTextPropertyModel

---
+++ owsi-alfresco/owsi-alfresco-component/owsi-alfresco-component-model/src/main/java/fr/openwide/alfresco/component/model/node/model/property/PropertyEnumeration.java
@@ -0,0 +1,9 @@
+package fr.openwide.alfresco.component.model.node.model.property;
+
+public interface PropertyEnumeration {
+
+	String OTHER_VALUES = "___ANY_OTHER_VALUE___";
+	
+	String getCode();
+
+}
