cb030d27d22da8bbeef4da78303f0cc1e14d9a9b
Adrien SAUVEZ <adrien.sauvez@openwide.fr>
2015-09-03 16:11:26 +0200

    Ne fait pas déconnexion deux fois

--- owsi-alfresco/owsi-alfresco-app/owsi-alfresco-app-web/src/main/java/fr/openwide/alfresco/app/web/security/authentication/RepositoryLogoutHandler.java
+++ owsi-alfresco/owsi-alfresco-app/owsi-alfresco-app-web/src/main/java/fr/openwide/alfresco/app/web/security/authentication/RepositoryLogoutHandler.java
@@ -1,14 +1,10 @@
 package fr.openwide.alfresco.app.web.security.authentication;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.ApplicationListener;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.web.authentication.logout.LogoutHandler;
 import org.springframework.security.web.session.HttpSessionDestroyedEvent;
 
 import fr.openwide.alfresco.api.core.remote.exception.AccessDeniedRemoteException;
@@ -21,7 +17,7 @@ import fr.openwide.alfresco.app.core.security.service.RepositoryAuthenticationUs
  *  	<listener-class>org.springframework.security.web.session.HttpSessionEventPublisher</listener-class>
  * </listener>
 */
-public class RepositoryLogoutHandler implements LogoutHandler, ApplicationListener<HttpSessionDestroyedEvent> {
+public class RepositoryLogoutHandler implements ApplicationListener<HttpSessionDestroyedEvent> {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(RepositoryLogoutHandler.class);
 
@@ -31,31 +27,16 @@ public class RepositoryLogoutHandler implements LogoutHandler, ApplicationListen
 		this.userDetailsService = userDetailsService;
 	}
 
-	/**
-	 * Appeler sur un logout explicite.
-	 */
-	@Override
-	public void logout(HttpServletRequest request, HttpServletResponse response, Authentication authentication) {
-		logoutRepository(authentication);
-	}
-
-	/**
-	 * Appeler sur un session timeout. Logout le ticket Alfresco.
-	 */
 	@Override
 	public void onApplicationEvent(HttpSessionDestroyedEvent event) {
 		for (SecurityContext context : event.getSecurityContexts()) {
 			Authentication authentication = context.getAuthentication();
-			logoutRepository(authentication);
-		}
-	}
-	
-	private void logoutRepository(Authentication authentication) {
-		if (authentication != null) {
-			try {
-				userDetailsService.logout(authentication);
-			} catch (AccessDeniedRemoteException e) {
-				LOGGER.warn("Could not logout authentication on the repository: {}", authentication);
+			if (authentication != null) {
+				try {
+					userDetailsService.logout(authentication);
+				} catch (AccessDeniedRemoteException e) {
+					LOGGER.warn("Could not logout authentication on the repository: {}", authentication);
+				}
 			}
 		}
 	}
