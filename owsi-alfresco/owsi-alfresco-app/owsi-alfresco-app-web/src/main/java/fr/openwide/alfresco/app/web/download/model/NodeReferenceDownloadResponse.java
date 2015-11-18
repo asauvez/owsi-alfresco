83dc5aceb57e0f22179264bb2930f416b2c8a5d3
Adrien SAUVEZ <adrien.sauvez@openwide.fr>
2015-11-18 16:12:51 +0100

    Ajoute autres types de rendition

--- owsi-alfresco/owsi-alfresco-app/owsi-alfresco-app-web/src/main/java/fr/openwide/alfresco/app/web/download/model/NodeReferenceDownloadResponse.java
+++ owsi-alfresco/owsi-alfresco-app/owsi-alfresco-app-web/src/main/java/fr/openwide/alfresco/app/web/download/model/NodeReferenceDownloadResponse.java
@@ -7,7 +7,19 @@ import fr.openwide.alfresco.api.core.remote.model.NodeReference;
 
 public class NodeReferenceDownloadResponse extends DownloadResponse {
 
+	// cf. thumbnail-service-context.xml
+	// 100x100 
+	private static final NameReference MEDIUM_PREVIEW_RENDITION = NameReference.create("cm", "medium");
+	// 100x100 sans enlargement
+	private static final NameReference DOCLIB_PREVIEW_RENDITION = NameReference.create("cm", "doclib");
+	// 960x960
 	private static final NameReference IMG_PREVIEW_RENDITION = NameReference.create("cm", "imgpreview");
+	// User avatar 64x64 image thumbnail options
+	private static final NameReference AVATAR_PREVIEW_RENDITION = NameReference.create("cm", "avatar");
+	// User avatar 32x32 image thumbnail options
+	private static final NameReference AVATAR32_PREVIEW_RENDITION = NameReference.create("cm", "avatar32");
+	// shockwave-flash
+	private static final NameReference WEB_PREVIEW_RENDITION = NameReference.create("cm", "webpreview");
 
 	private static final NameReference DEFAULT_CONTENT_PROPERTY = NameReference.create("cm", "content");
 	
@@ -42,7 +54,22 @@ public class NodeReferenceDownloadResponse extends DownloadResponse {
 		this.renditionName = renditionName;
 		return this;
 	}
+	public NodeReferenceDownloadResponse renditionMediumPreview() {
+		return renditionName(MEDIUM_PREVIEW_RENDITION);
+	}
+	public NodeReferenceDownloadResponse renditionDocLibPreview() {
+		return renditionName(DOCLIB_PREVIEW_RENDITION);
+	}
 	public NodeReferenceDownloadResponse renditionImgPreview() {
 		return renditionName(IMG_PREVIEW_RENDITION);
+	}
+	public NodeReferenceDownloadResponse renditionAvatarPreview() {
+		return renditionName(AVATAR_PREVIEW_RENDITION);
+	}
+	public NodeReferenceDownloadResponse renditionAvatar32Preview() {
+		return renditionName(AVATAR32_PREVIEW_RENDITION);
+	}
+	public NodeReferenceDownloadResponse renditionWebPreview() {
+		return renditionName(WEB_PREVIEW_RENDITION);
 	}
 }
