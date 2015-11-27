c9fe162087ee1a729183f1f54e46e9b8c3cff371
Adrien SAUVEZ <adrien.sauvez@openwide.fr>
2015-11-27 09:39:03 +0100

    RepositoryAuthorityType

--- owsi-alfresco/owsi-alfresco-app/owsi-alfresco-app-core/src/main/java/fr/openwide/alfresco/app/core/node/model/NodeReferenceType.java
+++ owsi-alfresco/owsi-alfresco-app/owsi-alfresco-app-core/src/main/java/fr/openwide/alfresco/app/core/node/model/NodeReferenceType.java
@@ -1,85 +1,17 @@
 package fr.openwide.alfresco.app.core.node.model;
 
-import java.io.Serializable;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Types;
-import java.util.Objects;
-
-import org.hibernate.HibernateException;
-import org.hibernate.engine.spi.SessionImplementor;
-import org.hibernate.type.StringType;
-import org.hibernate.usertype.UserType;
-
 import fr.openwide.alfresco.api.core.remote.model.NodeReference;
 
-public class NodeReferenceType implements UserType {
+public class NodeReferenceType extends AbstractStringUserType<NodeReference> {
 
 	@Override
-	public int[] sqlTypes() {
-		return new int[] { Types.VARCHAR };
-	}
-
-	@Override
-	public boolean isMutable() {
-		return false;
-	}
-
-	@Override
-	public Class<?> returnedClass() {
+	public Class<NodeReference> returnedClass() {
 		return NodeReference.class;
 	}
 
 	@Override
-	public Object nullSafeGet(ResultSet rs, String[] names, SessionImplementor session, Object owner)
-			throws HibernateException, SQLException {
-		String value = (String) StringType.INSTANCE.get(rs, names[0], session);
-		if (value == null) {
-			return null;
-		} else {
-			return NodeReference.create(value);
-		}
-	}
-
-	@Override
-	public void nullSafeSet(PreparedStatement st, Object value, int index, SessionImplementor session) 
-			throws HibernateException, SQLException {
-		if (value == null) {
-			StringType.INSTANCE.set(st, null, index, session);
-		} else {
-			StringType.INSTANCE.set(st, value.toString(), index, session);
-		}
-	}
-
-	@Override
-	public boolean equals(Object x, Object y) throws HibernateException {
-		return Objects.equals(x, y);
-	}
-
-	@Override
-	public int hashCode(Object x) throws HibernateException {
-		return Objects.hashCode(x);
-	}
-
-	@Override
-	public Object deepCopy(Object value) throws HibernateException {
-		return value; // type is immutable
-	}
-
-	@Override
-	public Serializable disassemble(Object value) throws HibernateException {
-		return (Serializable) value;
-	}
-
-	@Override
-	public Object assemble(Serializable cached, Object owner) throws HibernateException {
-		return cached;
-	}
-
-	@Override
-	public Object replace(Object original, Object target, Object owner) throws HibernateException {
-		return original;
+	protected NodeReference getAsObject(String value) {
+		return NodeReference.create(value);
 	}
 
 }
