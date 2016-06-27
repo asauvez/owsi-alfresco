37726ae26bab8aee298e5fa54cc964d6bfc44288
Remi Colas <remi.colas@smile.fr>
2016-06-27 17:59:59 +0200

    Ajout du model qshare

--- owsi-alfresco/owsi-alfresco-component/owsi-alfresco-component-model/src/main/java/fr/openwide/alfresco/component/model/repository/model/QShareModel.java
+++ owsi-alfresco/owsi-alfresco-component/owsi-alfresco-component-model/src/main/java/fr/openwide/alfresco/component/model/repository/model/QShareModel.java
@@ -3,7 +3,7 @@ package fr.openwide.alfresco.component.model.repository.model;
 import fr.openwide.alfresco.api.core.remote.model.NamespaceReference;
 import fr.openwide.alfresco.component.model.repository.model.qshare.Shared;
 
-public interface QShare {
+public interface QShareModel {
 
 	// https://svn.alfresco.com/repos/alfresco-open-mirror/alfresco/HEAD/root/projects/repository/config/alfresco/model/quickShareModel.xml
 	NamespaceReference NAMESPACE = NamespaceReference.create("qshare", "http://www.alfresco.org/model/qshare/1.0");
