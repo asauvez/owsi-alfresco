b8c12a5d0324cb7e79e2f51a9fb1032b8b21cd8a
Adrien SAUVEZ <adrien.sauvez@openwide.fr>
2017-06-21 09:14:26 +0200

    RepositoryUserProvider plus utilisé

--- owsi-alfresco/owsi-alfresco-app/owsi-alfresco-app-core/src/main/java/fr/openwide/alfresco/app/core/authentication/model/RepositoryUserProvider.java
+++
@@ -1,9 +0,0 @@
-package fr.openwide.alfresco.app.core.authentication.model;
-
-import fr.openwide.alfresco.api.core.authentication.model.RepositoryUser;
-
-public interface RepositoryUserProvider {
-
-	RepositoryUser getRepositoryUser();
-
-}
