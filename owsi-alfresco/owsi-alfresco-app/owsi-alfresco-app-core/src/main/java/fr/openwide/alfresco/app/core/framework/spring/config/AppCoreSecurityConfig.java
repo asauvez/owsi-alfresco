9d2dc912fb9c91606cba354d30b147903db48663
Adrien SAUVEZ <adrien.sauvez@openwide.fr>
2017-07-28 10:40:26 +0200

    Simplifie AppCoreServiceConfig

--- owsi-alfresco/owsi-alfresco-app/owsi-alfresco-app-core/src/main/java/fr/openwide/alfresco/app/core/framework/spring/config/AppCoreSecurityConfig.java
+++ owsi-alfresco/owsi-alfresco-app/owsi-alfresco-app-core/src/main/java/fr/openwide/alfresco/app/core/framework/spring/config/AppCoreSecurityConfig.java
@@ -1,16 +1,31 @@
 package fr.openwide.alfresco.app.core.framework.spring.config;
 
+import java.util.Map;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.security.access.hierarchicalroles.RoleHierarchy;
 import org.springframework.security.access.intercept.RunAsImplAuthenticationProvider;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import fr.openwide.alfresco.api.core.node.binding.content.NodeContentSerializationComponent;
+import fr.openwide.alfresco.api.core.node.binding.content.NodeContentSerializer;
+import fr.openwide.alfresco.api.core.node.binding.content.serializer.ByteArrayRepositoryContentSerializer;
 import fr.openwide.alfresco.app.core.authentication.service.AuthenticationService;
+import fr.openwide.alfresco.app.core.authentication.service.impl.AuthenticationServiceImpl;
+import fr.openwide.alfresco.app.core.node.binding.MultipartFileRepositoryContentSerializer;
+import fr.openwide.alfresco.app.core.remote.service.impl.RepositoryRemoteBinding;
+import fr.openwide.alfresco.app.core.remote.service.impl.RepositoryRemoteExceptionHandler;
 import fr.openwide.alfresco.app.core.security.service.RepositoryAuthenticationUserDetailsService;
 import fr.openwide.alfresco.app.core.security.service.TicketReferenceProvider;
 import fr.openwide.alfresco.app.core.security.service.RunAsUserManager;
@@ -25,16 +40,25 @@ public class AppCoreSecurityConfig {
 
 	private static final String RUN_AS_SHARED_KEY = UUID.randomUUID().toString();
 
-	@Autowired
-	private AuthenticationService authenticationService;
-	
+	@Autowired private Environment environment;
+
 	@Autowired(required=false)
 	@Qualifier(AppCorePermissionConfigurerAdapter.LOGIN_TIME_ROLE_HIERARCHY)
 	public RoleHierarchy loginTimeRoleHierarchy;
 	
 	@Bean
+	public AuthenticationService authenticationService() {
+		String authenticationHeader = environment.getRequiredProperty("application.authentication.repository.header.name");
+		return new AuthenticationServiceImpl(
+				unauthenticatedRepositoryRemoteBinding(), 
+				requiringExplicitTicketRemoteBinding(),
+				authenticationRemoteBinding(), 
+				authenticationHeader);
+	}
+	
+	@Bean
 	public RepositoryAuthenticationUserDetailsService repositoryAuthenticationUserDetailsService() {
-		return new RepositoryAuthenticationUserDetailsServiceImpl(authenticationService, loginTimeRoleHierarchy);
+		return new RepositoryAuthenticationUserDetailsServiceImpl(authenticationService(), loginTimeRoleHierarchy);
 	}
 
 	/**
@@ -82,4 +106,59 @@ public class AppCoreSecurityConfig {
 		return RUN_AS_SHARED_KEY;
 	}
 
+	// ----- RepositoryRemoteBinding ---------------------------------------------------------
+	
+	@Bean
+	@Primary
+	public RepositoryRemoteBinding userAwareRepositoryRemoteBinding() {
+		String repositoryUri = environment.getRequiredProperty("application.repository.root.uri");
+		String ticketName = environment.getRequiredProperty("application.repository.ticket.name");
+		return new RepositoryRemoteBinding(remoteRestTemplate(), serializationComponent(), repositoryUri, ticketName, null, ticketProvider());
+	}
+
+	@Bean
+	public RepositoryRemoteBinding unauthenticatedRepositoryRemoteBinding() {
+		String repositoryUri = environment.getRequiredProperty("application.repository.root.uri");
+		return new RepositoryRemoteBinding(remoteRestTemplate(), serializationComponent(), repositoryUri);
+	}
+
+	@Bean
+	public RepositoryRemoteBinding requiringExplicitTicketRemoteBinding() {
+		String repositoryUri = environment.getRequiredProperty("application.repository.root.uri");
+		String ticketName = environment.getRequiredProperty("application.repository.ticket.name");
+		return new RepositoryRemoteBinding(remoteRestTemplate(), serializationComponent(), repositoryUri, ticketName);
+	}
+
+	@Bean
+	public RepositoryRemoteBinding authenticationRemoteBinding() {
+		String authenticationUri = environment.getRequiredProperty("application.authentication.repository.root.uri");
+		return new RepositoryRemoteBinding(remoteRestTemplate(), serializationComponent(), authenticationUri);
+	}
+
+	@Bean
+	public RepositoryRemoteBinding shareRemoteBinding() {
+		String repositoryUri = environment.getRequiredProperty("application.share.root.uri");
+		String ticketName = environment.getRequiredProperty("application.share.ticket.name");
+		return new RepositoryRemoteBinding(remoteRestTemplate(), serializationComponent(), repositoryUri, null, ticketName, ticketProvider());
+	}
+
+	@Bean
+	public RestTemplate remoteRestTemplate() {
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.setErrorHandler(new RepositoryRemoteExceptionHandler());
+		// do not buffer request objects (not to run out of memory on file upload...)
+		((SimpleClientHttpRequestFactory) restTemplate.getRequestFactory()).setBufferRequestBody(false);
+		return restTemplate;
+	}
+
+	@Bean
+	public NodeContentSerializationComponent serializationComponent() {
+		Map<Class<?>, NodeContentSerializer<?>> serializersByClass = NodeContentSerializationComponent.getDefaultSerializersByClass();
+		serializersByClass.put(MultipartFile.class, MultipartFileRepositoryContentSerializer.INSTANCE);
+
+		return new NodeContentSerializationComponent(
+				new ObjectMapper(), 
+				serializersByClass, 
+				ByteArrayRepositoryContentSerializer.INSTANCE);
+	}
 }
--- owsi-alfresco/owsi-alfresco-app/owsi-alfresco-app-core/src/main/java/fr/openwide/alfresco/app/core/framework/spring/config/AppCoreServiceConfig.java
+++ owsi-alfresco/owsi-alfresco-app/owsi-alfresco-app-core/src/main/java/fr/openwide/alfresco/app/core/framework/spring/config/AppCoreServiceConfig.java
@@ -3,24 +3,15 @@ package fr.openwide.alfresco.app.core.framework.spring.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Import;
-import org.springframework.core.env.Environment;
 
-import fr.openwide.alfresco.api.core.authority.service.AuthorityRemoteService;
-import fr.openwide.alfresco.api.core.node.service.NodeRemoteService;
-import fr.openwide.alfresco.api.core.search.service.NodeSearchRemoteService;
-import fr.openwide.alfresco.app.core.authentication.service.AuthenticationService;
-import fr.openwide.alfresco.app.core.authentication.service.impl.AuthenticationServiceImpl;
 import fr.openwide.alfresco.app.core.authority.service.AuthorityService;
 import fr.openwide.alfresco.app.core.authority.service.impl.AuthorityServiceImpl;
 import fr.openwide.alfresco.app.core.licence.service.LicenseService;
 import fr.openwide.alfresco.app.core.licence.service.impl.LicenseServiceImpl;
 import fr.openwide.alfresco.app.core.node.service.NodeService;
 import fr.openwide.alfresco.app.core.node.service.impl.NodeServiceImpl;
-import fr.openwide.alfresco.app.core.remote.service.impl.RepositoryRemoteBinding;
 import fr.openwide.alfresco.app.core.search.service.NodeSearchService;
 import fr.openwide.alfresco.app.core.search.service.impl.NodeSearchServiceImpl;
-import fr.openwide.alfresco.app.core.security.service.TicketReferenceProvider;
 import fr.openwide.alfresco.app.core.site.service.SiteService;
 import fr.openwide.alfresco.app.core.site.service.impl.SiteServiceImpl;
 import fr.openwide.alfresco.component.model.authority.service.AuthorityModelService;
@@ -31,62 +22,47 @@ import fr.openwide.alfresco.component.model.search.service.NodeSearchModelServic
 import fr.openwide.alfresco.component.model.search.service.impl.NodeSearchModelServiceImpl;
 
 @Configuration
-@Import(AppCoreRemoteBindingConfig.class)
 public class AppCoreServiceConfig {
 
 	@Autowired
-	private AppCoreRemoteBindingConfig appCoreRemoteBindingConfig;
-	
-	@Autowired
-	private Environment environment;
+	private AppCoreSecurityConfig appCoreSecurityConfig;
 
 	@Bean
-	public AuthenticationService authenticationService() {
-		String authenticationHeader = environment.getRequiredProperty("application.authentication.repository.header.name");
-		return new AuthenticationServiceImpl(
-				appCoreRemoteBindingConfig.unauthenticatedRepositoryRemoteBinding(), 
-				appCoreRemoteBindingConfig.requiringExplicitTicketRemoteBinding(),
-				appCoreRemoteBindingConfig.authenticationRemoteBinding(), 
-				authenticationHeader);
+	public NodeService nodeService() {
+		return new NodeServiceImpl(appCoreSecurityConfig.userAwareRepositoryRemoteBinding());
+	}
+	@Bean
+	public NodeModelService nodeModelService() {
+		return new NodeModelServiceImpl(nodeService());
 	}
 
 	@Bean
-	public NodeService nodeService(RepositoryRemoteBinding repositoryRemoteBinding) {
-		return new NodeServiceImpl(repositoryRemoteBinding);
+	public AuthorityService authorityService() {
+		return new AuthorityServiceImpl(nodeService());
 	}
 	@Bean
-	public NodeModelService nodeModelService(NodeRemoteService nodeService) {
-		return new NodeModelServiceImpl(nodeService);
-	}
-
-	@Bean
-	public AuthorityService authorityService(NodeService nodeService) {
-		return new AuthorityServiceImpl(nodeService);
-	}
-	@Bean
-	public AuthorityModelService authorityModelService(AuthorityRemoteService authorityService) {
-		return new AuthorityModelServiceImpl(authorityService);
+	public AuthorityModelService authorityModelService() {
+		return new AuthorityModelServiceImpl(authorityService());
 	}
 
 	@Bean
-	public NodeSearchService nodeSearchService(NodeService nodeService) {
-		return new NodeSearchServiceImpl(nodeService);
+	public NodeSearchService nodeSearchService() {
+		return new NodeSearchServiceImpl(nodeService());
 	}
 	@Bean
-	public NodeSearchModelService nodeSearchModelService(NodeSearchRemoteService nodeSearchService) {
-		return new NodeSearchModelServiceImpl(nodeSearchService);
+	public NodeSearchModelService nodeSearchModelService() {
+		return new NodeSearchModelServiceImpl(nodeSearchService());
 	}
 
 	@Bean
-	public LicenseService licenceService(TicketReferenceProvider ticketProvider) {
-		return new LicenseServiceImpl(appCoreRemoteBindingConfig.userAwareRepositoryRemoteBinding(ticketProvider));
+	public LicenseService licenceService() {
+		return new LicenseServiceImpl(appCoreSecurityConfig.userAwareRepositoryRemoteBinding());
 	}
 	
 	@Bean
-	public SiteService siteService(AuthorityService authorityService, NodeSearchService nodeSearchService, 
-			TicketReferenceProvider ticketProvider) {
-		return new SiteServiceImpl(authorityService, nodeSearchService, 
-				appCoreRemoteBindingConfig.shareRemoteBinding(ticketProvider));
+	public SiteService siteService() {
+		return new SiteServiceImpl(authorityService(), nodeSearchService(), 
+				appCoreSecurityConfig.shareRemoteBinding());
 	}
 
 }
