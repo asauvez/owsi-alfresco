017e8a96ab96aff9491f0a16acec3673f82c6c72
Adrien SAUVEZ <adrien.sauvez@smile.fr>
2020-08-28 15:06:03 +0200

    Migration: Ne s'occupe que des jar, pas des zip

--- owsi-alfresco/owsi-alfresco-parent/owsi-alfresco-repo-migration-maven-plugin/src/main/java/fr/openwide/alfresco/repo/migrationtool/plugin/MigrationMojo.java
+++ owsi-alfresco/owsi-alfresco-parent/owsi-alfresco-repo-migration-maven-plugin/src/main/java/fr/openwide/alfresco/repo/migrationtool/plugin/MigrationMojo.java
@@ -154,7 +154,7 @@ public class MigrationMojo extends AbstractMigrationMojo {
 	
 	private void initDependency(Artifact artifact) throws Exception {
 		File file = artifact.getFile();
-		if (file != null && ! file.isDirectory()) {
+		if (file != null && ! file.isDirectory() && file.getName().endsWith(".jar")) {
 			ZipInputStream zip = new ZipInputStream(new BufferedInputStream(new FileInputStream(file)));
 			try {
 				ZipEntry entry;
