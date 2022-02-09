1cd49d9495aeb49bccca2fd5c5f6dcf5d3e02d5a
Adrien SAUVEZ <adrien.sauvez@smile.fr>
2022-02-09 17:32:50 +0100

    Genere model java pour owsi-module

---
+++ owsi-alfresco/owsi-alfresco-api/owsi-alfresco-api-module/src/main/java/fr/openwide/alfresco/api/module/model/package-info.java
@@ -0,0 +1,7 @@
+@GenerateJavaModel(
+	importModels = "../../owsi-alfresco-repo/owsi-alfresco-repo-module/src/main/config/model/owsiModel.xml",
+	useJackson = false
+)
+package fr.openwide.alfresco.api.module.model;
+
+import fr.openwide.alfresco.repo.wsgenerator.annotation.GenerateJavaModel;
